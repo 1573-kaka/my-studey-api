45db964cbf01f33d588ac1e5b9dea56e2348a3c5
liuhuan <ares1573@163.com>
2021-02-07 20:07:31 +0800

    fix

---
+++ src/test/java/com/imust/crm/test/TestDemo.java
@@ -0,0 +1,15 @@
+package com.imust.crm.test;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+public class TestDemo {
+    @Test
+    public void test() {
+        System.out.println("你好，李焕英");
+        new Date();
+        new Date();
+        int a;
+    }
+}
